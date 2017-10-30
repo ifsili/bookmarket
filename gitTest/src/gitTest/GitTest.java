f6d28063af291a3e9ec58b80ae5750b9d3e033f1
dahyun hwang <amsterdahn@gmail.com>
2017-10-30 17:36:27 +0900

    dd

--- gitTest/src/gitTest/GitTest.java
+++
@@ -1,7 +0,0 @@
-package gitTest;
-
-public class GitTest {
-public static void main(String[] args) {
-	System.out.println("WOW");
-}
-}
---
+++ gitgitgit/src/gitgitgit/GitGit.java
@@ -0,0 +1,8 @@
+package gitgitgit;
+
+public class GitGit {
+
+	public static void main(String[] args) {
+		System.out.println("please");
+	}
+}
