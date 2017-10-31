659442951fc15ebf7372d9c0a452743f6978ffb9
kangjihoon <kangjihoon@172.30.1.42>
2017-10-31 21:03:30 +0900

    deal model class

--- BookMarket_01/src/dto/Deal.java
+++ BookMarket_01/src/dto/Deal.java
@@ -1,5 +1,43 @@
 package dto;
 
+import java.util.Date;
+
 public class Deal {
 
+	private int deal_idx;
+	private int participant;
+	private Date deal_date;
+	private int deal_price;
+	
+	public int getDeal_idx() {
+		return deal_idx;
+	}
+	public void setDeal_idx(int deal_idx) {
+		this.deal_idx = deal_idx;
+	}
+	public int getParticipant() {
+		return participant;
+	}
+	public void setParticipant(int participant) {
+		this.participant = participant;
+	}
+	public Date getDeal_date() {
+		return deal_date;
+	}
+	public void setDeal_date(Date deal_date) {
+		this.deal_date = deal_date;
+	}
+	public int getDeal_price() {
+		return deal_price;
+	}
+	public void setDeal_price(int deal_price) {
+		this.deal_price = deal_price;
+	}
+	@Override
+	public String toString() {
+		return "Deal [deal_idx=" + deal_idx + ", participant=" + participant + ", deal_date=" + deal_date
+				+ ", deal_price=" + deal_price + "]";
+	}
+	
+	
 }
