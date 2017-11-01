9002a1fb42158798ef683cc80e37c26f20ffb2e5
dahyun hwang <amsterdahn@gmail.com>
2017-11-01 13:39:47 +0900

    writing action factory class - ing

--- BookMarket_01/src/controller/ActionFactory.java
+++ BookMarket_01/src/controller/ActionFactory.java
@@ -1,5 +1,30 @@
 package controller;
 
+import controller.action.Action;
+
 public class ActionFactory {
+
+	private static ActionFactory instance = null;
+
+	public static ActionFactory getInstance() {
+		if (instance == null) {
+			instance = new ActionFactory();
+		}
+		return instance;
+	}
+
+	private ActionFactory() {
+		super();
+	}
+
+	public Action getAction(String command) {
+		Action action = null;
+
+		if (command.equals("command in string")) {
+		}
+		System.out.println("ActionFactory: " + command);
+		return action;
+
+	}
 
 }
--- BookMarket_01/src/controller/MemberServlet.java
+++ BookMarket_01/src/controller/MemberServlet.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import controller.action.Action;
+
 /**
  * Servlet implementation class MemberServlet
  */
@@ -14,26 +16,28 @@ import javax.servlet.http.HttpServletResponse;
 public class MemberServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
-    public MemberServlet() {
-        // TODO Auto-generated constructor stub
-    }
-
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String command = request.getParameter("command");
+		ActionFactory af = ActionFactory.getInstance();
+		Action action = af.getAction(command);
+
+		if (action != null) {
+			action.execute(request, response);
+		}
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
 		doGet(request, response);
 	}
 
